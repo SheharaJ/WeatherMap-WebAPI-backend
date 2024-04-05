b04064ab1bd17f3e7e5923407056b9b123258585
SheharaJ <sheharajaya7@gmail.com>
2024-04-05 20:21:12 +0530

    datagenarator done

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const sql = require('mssql');
 
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 
 const config = {
   user: 'sjadmin',
@@ -29,8 +33,8 @@ app.get('/weather', async (req, res) => {
     // Connect to the database
     await sql.connect(config);
 
-    // Query to select all data from the districts table
-    const result = await sql.query`SELECT * FROM districts`;
+    // Query to select all data from the weather table
+    const result = await sql.query`SELECT * FROM weather`;
 
     // Close the database connection
     await sql.close();
@@ -43,15 +47,15 @@ app.get('/weather', async (req, res) => {
   }
 });
 
-// Endpoint to fetch data from the districts table based on district
+// Endpoint to fetch data from the weather table based on district
 app.get('/weather/:district', async (req, res) => {
   const district = req.params.district;
   try {
     // Connect to the database
     await sql.connect(config);
 
-    // Query to select data from the districts table based on district
-    const result = await sql.query`SELECT * FROM districts WHERE dname = ${district}`;
+    // Query to select data from the weather table based on district
+    const result = await sql.query`SELECT * FROM weather WHERE district = ${district}`;
 
     // Close the database connection
     await sql.close();
@@ -64,8 +68,32 @@ app.get('/weather/:district', async (req, res) => {
   }
 });
 
+// generate and insert random data into the database
 
+// const districts = ['Ampara', 'Anuradhapura', 'Bhadulla', 'Batticoloa', 'Colombo', 'Galle', 'Gampaha', 'Hambantota', 'Jaffna', 'Kalutara', 'Kandy'];
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// const generateRandomData = () => {
+//     const temperature = (Math.random() * (35 - 20) + 20).toFixed(2);
+//     const humidity = (Math.random() * (100 - 60) + 60).toFixed(2);
+//     const airPressure = (Math.random() * (1020 - 1000) + 1000).toFixed(2);
+//     const dist = districts[Math.floor(Math.random() * districts.length)]; // Randomly select a province
+
+//     const query = `UPDATE weather SET humidity=${humidity}, temp=${temperature}, air=${airPressure} WHERE district = ${dist}`;
+//     connection.query(query, [temperature, humidity, airPressure, province], (err, result) => {
+//         if (err) {
+//             console.error('Error inserting data:', err);
+//         } else {
+//             console.log('Weather data inserted successfully');
+//         }
+//     });
+// };
+
+// // Generate data every 5 minutes
+// setInterval(generateRandomData, 5 * 60 * 1000);
+
+// // Generate initial data immediately
+// generateRandomData();
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
